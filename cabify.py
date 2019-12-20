8755c6b8ddbae21bfe0d0c34a19376149c143253
betsyvies <ing.betsyvies@gmail.com>
2019-12-19 19:09:09 -0500

    change arrays to objects

--- cabify.py
+++ cabify.py
@@ -1,29 +1,4 @@
-import pytesseract
-import extcolors
-import requests
-from PIL import ImageFilter
-from PIL import Image
-from PIL import ImageFont
-from io import BytesIO
-import base64
-
-def process_image(img):
-  image = _get_image(img)
-  image.filter(ImageFilter.SHARPEN)
-  # Cabify = ((110, 91, 217), 1102)
-  # Beat = ((35, 210, 171), 9345)
-  # Uber = ((19, 19, 19), 355)
-  # Satelital = ((193, 29, 27), 4045)
-  # Indriver = ((22, 149, 44), 1780)
-  # pytesseract.image_to_string(image)
-  # print(_get_app(image))
-  return _get_app(image)
-
-def _get_image(img):
-  return Image.open(BytesIO(base64.b64decode(img)))
-
-def _get_app(image):
-  lista_strings = pytesseract.image_to_string(image).split()
+def cabify(lista_strings) :
   new_lista = []
 
   for string in lista_strings:
@@ -39,8 +14,6 @@ def _get_app(image):
     _get_data(new_lista[1][0], lista_strings[new_lista[1][1]:new_lista[2][1]]),
     _get_data(new_lista[2][0], lista_strings[new_lista[2][1]:])
   ]
-
-  # filter_string = list(filter(lambda string: string == 'Lite', lista_strings))
   return category_cabify
 
 def _get_data(category, lista):
